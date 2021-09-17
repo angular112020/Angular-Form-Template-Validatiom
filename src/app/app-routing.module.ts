3ecc0527d793d8b800bfb21c67ae55ff39e91358
admin <admin@CHETUIWK0393.chetu.com>
2021-09-17 19:11:18 +0530

    sep17

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthoGuard } from './authentication/autho.guard';
+import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
+import { EmployeeRegistrationComponent } from './components/pages/employee-registration/employee-registration.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { OrderMenuComponent } from './components/pages/order-menu/order-menu.component';
+
+const routes: Routes = [
+  {path:'', component:DashboardComponent,
+children:[
+  {path:'employee-registration', component:EmployeeRegistrationComponent},
+  {path:'order-menu', component:OrderMenuComponent}
+]
+
+},
+  {path:'login', component:LoginComponent},
+  // {path:'dashboard', component:DashboardComponent, canActivate:[AuthoGuard]}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
